bd62e893d1e52ae58b84c8e785cca4d85b6d9018
sanyasirao-surada <ssraome.1@gmail.com>
2025-03-01 11:05:36 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", function() {
+  // For now, simply log a welcome message.
+  console.log("Welcome to Mr. Surada Sanyasi Rao's Resume Website!");
+});
